84b110c0d3f6e5df260992b3385ff1138bb00950
andy <49623329+AndyThanhLe@users.noreply.github.com>
2024-07-19 17:17:21 -0400

    revise image viewer implementation

--- docs/assets/scripts/script.js
+++ docs/assets/scripts/script.js
@@ -7,12 +7,31 @@ $('.project-modal-close').click(function() {
   $(this).closest('.project-modal').toggleClass('hide');
 });
 
-$('#project-images-forward').click(function() {
-  let current = $(this).find('.visible');
-  let next = current.next();
+$('.project-images-back').click(function() {
+  let current = $('.visible');
+  let previous = current.prev();
+
+  if (previous.length == 0) {
+    previous = $('.project-images').children(':last-child');
+  }
+
+  current.toggleClass('hide');
   current.toggleClass('visible');
-  current.toggleClass('hidden');
-  next.toggleClass('visible');
-  next.toggleClass('hidden');
+  previous.toggleClass('hide');
+  previous.toggleClass('visible');
 });
 
+$('.project-images-forward').click(function() {
+  let images = $(this).siblings('.project-images');
+
+  let current = images.children('.visible');
+  let next = current.next();
+  if (next.length == 0) {
+    next = images.children(':first-child');
+  }
+
+  current.toggleClass('hide');
+  current.toggleClass('visible');
+  next.toggleClass('hide');
+  next.toggleClass('visible');
+});
